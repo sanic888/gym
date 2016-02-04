bb54cd7e92bdcbee5bb2990d11c97514c4dccf3f
unknown <ayancharuk@ayancharuk-1205.exadel.by>
2016-02-04 16:58:55 +0300

    added submit data by click enter

--- app/components/signin-form.js
+++ app/components/signin-form.js
@@ -3,18 +3,24 @@ import Ember from 'ember';
 const { service } = Ember.inject;
 
 export default Ember.Component.extend({
-  session: service('session'),
-
-  actions: {
-    authenticateWithOAuth2() {
-      let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error);
-      });
-    },
-
-    authenticateWithFacebook() {
-      this.get('session').authenticate('authenticator:torii', 'facebook');
-    }
-  }
+	session: service('session'),
+	keyPress(event) {
+		if (event.which == 13 || event.keyCode == 13) {
+			this._authenticateWithOAuth2();
+		}
+	},
+	_authenticateWithOAuth2() {
+		let { identification, password } = this.getProperties('identification', 'password');
+		this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
+			this.set('errorMessage', reason.error);
+		});
+	},
+	actions: {
+		authenticateWithOAuth2() {
+			this._authenticateWithOAuth2();
+		},
+		authenticateWithFacebook() {
+			this.get('session').authenticate('authenticator:torii', 'facebook');
+		}
+	}
 });
